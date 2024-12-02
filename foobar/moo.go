cf9afa6776700fe0d6b03abf1ec4721bdcd55abb
dave <dave@cyllective.com>
2024-12-02 18:04:34 +0100

    wip

---
+++ foobar/moo.go
@@ -0,0 +1,18 @@
+package foobar
+
+import "fmt"
+
+func moo() int {
+	if 1 <= (3*44) && 1 > 99 && 1 == 3 && 2 < 1 && 3 > 0 {
+		if 1 > 03 {
+			fmt.Println("x")
+		} else {
+			if 1 <= (3*44) && 1 > 99 && 1 == 3 && 2 < 1 && 3 > 0 {
+				{
+					return 0
+				}
+			}
+		}
+	}
+	return 1
+}
